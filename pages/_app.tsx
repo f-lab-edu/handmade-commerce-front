05ae223776a3b9925114455831f9b5a03810ebc3
seula <seula0421@gmail.com>
2022-09-19 17:42:42 +0900

    mui 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useState } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import ErrorBoundary from '../src/shared/error/ErrorBoundary'
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ErrorBoundary from "../src/shared/error/ErrorBoundary";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient({defaultOptions: {
-    queries: {
-      retry:0,
-      suspense: true,
-      useErrorBoundary: true
-    },
-  },}))
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 0,
+            suspense: true,
+            useErrorBoundary: true,
+          },
+        },
+      })
+  );
 
   return (
-    
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        {/* <CssBaseline /> */}
         <ErrorBoundary>
           <Component {...pageProps} />
         </ErrorBoundary>
       </Hydrate>
       <ReactQueryDevtools />
     </QueryClientProvider>
-    
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
