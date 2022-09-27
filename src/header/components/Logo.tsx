43d8a2c5243541980abfd30c7d0897010f725ce7
seula <seula0421@gmail.com>
2022-09-27 22:45:23 +0900

    feat: 로고 이미지 Link 추가

--- src/header/components/Logo.tsx
+++ src/header/components/Logo.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import LogoUrl from "../../images/Logo.png";
 import Image from "next/image";
+import Link from "next/link";
+import { css } from "@emotion/react";
 
 type Props = {};
 
 const Logo = (props: Props) => {
   return (
-    <div>
-      <Image src={LogoUrl} alt="logo" width={200} height={80} />
-    </div>
+    <Link href={"/"}>
+      <div>
+        <a>
+          <Image
+            src={LogoUrl}
+            alt="logo"
+            width={200}
+            height={80}
+            css={css`
+              cursor: pointer;
+            `}
+          />
+        </a>
+      </div>
+    </Link>
   );
 };
 
