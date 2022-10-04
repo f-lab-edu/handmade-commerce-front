0472a358083be88f4d81d533d851067da40d4d68
seula <seula0421@gmail.com>
2022-10-04 20:53:48 +0900

    test;

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   concurrentFeatures: true,
   swcMinify: true,
   images: {
+    loader: "akamai",
+    path: "",
     domains: ["image.wconcept.co.kr", "goldendew.com", "ai.esmplus.com"],
   },
 };
